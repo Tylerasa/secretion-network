7f2be7830e82b2f5aae301a6f2e82b2af5ac86cd
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-04 13:35:22 +0000

    postcard update

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
+import React from "react";
+// import CustomLoader from "../CustomLoader/CustomLoader";
+import profile from "../../assets/images/profile.jpg";
+import "./styles.css";
 const PostCard = () => {
   return (
-    <div>PostCard</div>
-  )
-}
+    <div className="post-card">
+      {/* <CustomLoader /> */}
+      <img src={profile} className="post-image" />
+      <div>
+        <span className="username">username </span>
+        <span className="username">This is my post</span>
+      </div>
+    </div>
+  );
+};
 
-export default PostCard
+export default PostCard;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,11 +3,14 @@ import PostCard from "../../components/PostCard/PostCard";
 import "./styles.css";
 const Home = () => {
   return (
-    <div className="wrapper">
-      <div className="left-side">
-        <PostCard />
+    <div>
+      <div className="header"></div>
+      <div className="wrapper">
+        <div className="left-side">
+          <PostCard />
+        </div>
+        <div>side</div>
       </div>
-      <div>side</div>
     </div>
   );
 };
