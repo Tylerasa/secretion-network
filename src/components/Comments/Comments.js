ec30cf73c7f8b96d0001262d9270a7ff9e352144
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-04 14:44:19 +0000

    + comment modal + comment  card

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./styles.css";
+import profile from "../../assets/images/profile.jpg";
+
+const Comments = () => {
+  return (
+    <div className="comments-modal">
+      <div className="comment-card">
+        <div
+          className="comment-img"
+          style={{
+            background: `url(${profile})`,
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+            backgroundSize: "cover"
+          }}
+        ></div>
+        <div className="comments-main">
+          <div className="post-card-header">
+            <img src={profile} className="avatar" />
+            username
+          </div>
+          <div className="comment-line">
+            <img src={profile} className="avatar" />
+            username<span className="user-comment">this is a comment</span>
+          </div>
+          <div className="comment-line">
+            <img src={profile} className="avatar" />
+            username
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Comments;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Comments from "../../components/Comments/Comments";
 import PostCard from "../../components/PostCard/PostCard";
 import "./styles.css";
 const Home = () => {
   return (
     <div>
+      <Comments />
+
       <div className="header"></div>
       <div className="wrapper">
         <div className="left-side">
