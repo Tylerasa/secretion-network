8b58c7c33631d4884dbda33434258ecd3be1f36f
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-05 20:22:19 +0000

    + add post grid

--- src/components/AddPost/AddPost.js
+++ src/components/AddPost/AddPost.js
@@ -1,8 +1,11 @@
 import React, { useContext, useRef, useEffect, useState } from "react";
-import './styles.css'
+import FileBase64 from "react-file-base64";
+import "./styles.css";
 const AddPost = () => {
   const cardRef = useRef();
   const [clickState, setClickState] = useState(false);
+  const [picture, setPicture] = useState(null);
+  var picker = document.getElementsByTagName("input")[0];
 
   //   useEffect(() => {
   //     function handleClickOutside(event) {
@@ -19,20 +22,27 @@ const AddPost = () => {
   //       document.removeEventListener("mousedown", handleClickOutside);
   //     };
   //   }, [clickState, toggleComments]);
+  useEffect(() => {
+    if (picker) {
+      picker.style.visibility = "hidden";
+    }
+  }, []);
+  function getFile(file) {
+    setPicture(file);
+  }
   return (
     <div onClick={() => setClickState(!clickState)} className="comments-modal">
       <div ref={cardRef} className="comment-card">
         <div
           className="comment-img add-post"
-        //   style={{
-        //     background: `url(${profile})`,
-        //     backgroundRepeat: "no-repeat",
-        //     backgroundPosition: "center",
-        //     backgroundSize: "cover"
-        //   }}
-
+          style={{
+            background: picture ? `url(${picture.base64})` : null
+          }}
         >
-          click here to add a picture
+          {!picture ? (
+            <FileBase64 style={{ visibility: "collapse" }} onDone={getFile} />
+          ) : null}
+          {/* click here to add a picture */}
         </div>
         <div className="comments-main">
           <form
