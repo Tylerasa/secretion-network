42110bfa71e983a8f0fe4f4227fa0341dd024bc9
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-05 08:53:58 +0000

    + useContext

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -2,7 +2,11 @@ import React from "react";
 // import CustomLoader from "../CustomLoader/CustomLoader";
 import profile from "../../assets/images/profile.jpg";
 import "./styles.css";
-const PostCard = () => {
+const PostCard = (clickView) => {
+  const handleClick = () => {
+    console.log("clicked");
+    clickView(true);
+  };
   return (
     <div className="post-card">
       <div className="post-card-header">
@@ -13,7 +17,9 @@ const PostCard = () => {
       <div className="post-card-body">
         <span className="username">username </span>
         <span className="post-caption">This is my post</span>
-        <p className="view-comments">View all comments</p>
+        <p onClick={handleClick} className="view-comments">
+          View all comments
+        </p>
       </div>
     </div>
   );
