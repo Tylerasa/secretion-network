994adeb7d7dae46c4c7a33ff731cc9fefa70c5c2
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-05 10:11:41 +0000

    + .env file

--- src/pages/Home/Login/Login.js
+++ src/pages/Home/Login/Login.js
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./styles.css";
 const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const handleSubmit = (e) => {
     console.log("submitted");
     e.preventDefault();
+    var data = JSON.stringify({
+      username,
+      password
+    });
+
+    var config = {
+      method: "post",
+      url: "users/login",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   };
   return (
     <div className="login-wrapper">
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input placeholder="username" type="text" />
+        <input
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="username"
+          type="text"
+        />
         <br />
-        <input placeholder="password" type="password" />
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="password"
+          type="password"
+        />
         <br />
         <button>Click</button>
       </form>
