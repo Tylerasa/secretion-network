650a120ab3b4ca08c8a6b39124806750fd137da7
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-05 08:53:39 +0000

    + useContext

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState, createContext } from "react";
 import Comments from "../../components/Comments/Comments";
 import PostCard from "../../components/PostCard/PostCard";
 import "./styles.css";
 const Home = () => {
+  const [showComments, toggleComments] = useState(false);
+  const ShowContext = createContext()
   return (
-    <div>
-      <Comments />
-
+    <ShowContext.Provider value={showComments}>
+        <div>
+      {showComments ? <Comments /> : null}
       <div className="header"></div>
       <div className="wrapper">
         <div className="left-side">
@@ -16,6 +18,7 @@ const Home = () => {
         <div>side</div>
       </div>
     </div>
+    </ShowContext.Provider>
   );
 };
 
