923b80ce7e30904e6a2c915f61e3b50bae165a6f
SylvesterSarpong <sarpongasaresylvester@gmail.com>
2022-03-05 14:22:06 +0000

    + setting user id in localStorage

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -7,6 +7,7 @@ import UserComment from "./UserComment";
 const Comments = () => {
   const [showComments, toggleComments] = useContext(ShowContext);
   const [clickState, setClickState] = useState(false);
+  const [content, setContent] = useState("");
   const cardRef = useRef();
 
   useEffect(() => {
@@ -24,6 +25,31 @@ const Comments = () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [clickState, toggleComments]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    var data = JSON.stringify({
+      content
+    });
+    var config = {
+      method: "post",
+      url: `${process.env.REACT_APP_BE}/posts/add-comment/${ele}/621fcf5bd7310d25814bb469`,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("my_user_token")}`
+      },
+      data: data
+    };
+
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <div onClick={() => setClickState(!clickState)} className="comments-modal">
@@ -45,8 +71,9 @@ const Comments = () => {
           {showComments.post.comments.map((ele, i) => {
             return <UserComment key={i} item={ele} />;
           })}
-          <form className="form">
+          <form onSubmit={(e) => handleSubmit(e)} className="form">
             <input
+              onChange={(e) => setContent(e.target.value)}
               placeholder="say something..."
               className="form-input"
               type="text"
